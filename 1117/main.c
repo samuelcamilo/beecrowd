75b5bcd8eee473d1d39eaf663dcae5564cadc49c
Samuel Camilo <ssamuelcamilo@gmail.com>
2018-07-16 20:33:55 -0300

    Problema 1117 resolvido

--- 1117/main.c
+++ 1117/main.c
@@ -5,18 +5,38 @@ int main()
 {
     float nota01;
     float nota02;
+    int ehValidoNota = 0;
+    int ehValidoNota1 = 0;
+    int ehValidoNota2 = 0;
 
     scanf("%f", &nota01);
     scanf("%f", &nota02);
 
-    //while((nota01 >= 0 && nota01 <= 10) && (nota02 >= 0 && nota02 <= 10)){
-        //printf("teste!");
-    //}
+    while(ehValidoNota == 0){
 
-    if((nota01 >= 0 && nota01 <= 10) && (nota02 >= 0 && nota02 <= 10)){
-        printf("nota valida!");
-    }else{
-        printf("nota invalida!");
+        if(ehValidoNota1 == 0){
+            if(nota01 >= 0 && nota01 <= 10){
+                ehValidoNota1 = 1;
+            }else{
+                printf("nota invalida\n");
+                scanf("%f", &nota01);
+            }
+        }
+
+        if(ehValidoNota2 == 0){
+            if(nota02 >= 0 && nota02 <= 10){
+                ehValidoNota2 = 1;
+            }else{
+                printf("nota invalida\n");
+                scanf("%f", &nota02);
+            }
+        }
+
+        if(ehValidoNota1 == 1 && ehValidoNota2 == 1){
+            printf("media = %.2f\n", (nota01+nota02)/2);
+            ehValidoNota = 1;
+        }
+
     }
 
     return 0;
