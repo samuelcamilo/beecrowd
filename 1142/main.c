2ec6d44450e25c417c4c3d2a7717ed22f9075fe3
Samuel Camilo <ssamuelcamilo@gmail.com>
2018-07-26 21:52:59 -0300

    Solução do problema 1142

---
+++ 1142/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int entrada = 7;
+    int PUM = 4;
+    int FIM = 1;
+    int aux = 1;
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i <= entrada; i++){
+        for(j = aux; j <= (PUM*i); j++){
+            aux = j;
+            if(j == (PUM*i)){
+                printf("PUM");
+            }else{
+                printf("%d ", j);
+            }
+        }
+        aux++;
+        printf("\n");
+    }
+
+    return 0;
+}
