94d9808a665918e0ca6e92dc13f07fb10c2cc181
Samuel Camilo <ssamuelcamilo@gmail.com>
2018-10-26 02:11:42 -0300

    solução do problema 1164

--- 1164/main.c
+++ 1164/main.c
@@ -3,16 +3,35 @@
 
 int main()
 {
-    int n;
-    int cont = 0;
-    int valor;
+    int qt_teste = 0;
+    int i = 0;
 
-    scanf("%d", &n);
-    while(cont < n){
+    int entrada;
+    int contador = 1;
+    int somatorio = 0;
 
-        scanf("%d", &valor);
+    scanf("%d", &qt_teste);
+    while(i < qt_teste){
 
-        cont++;
+        scanf("%d", &entrada);
+        while(contador < entrada){
+
+            if(entrada % contador == 0){
+                somatorio = somatorio + contador;
+            }
+            contador++;
+        }
+
+
+        if(somatorio == entrada){
+            printf("%d eh perfeito\n", entrada);
+        }else{
+            printf("%d nao eh perfeito\n", entrada);
+        }
+
+        somatorio = 0;
+        contador = 1;
+        i++;
     }
 
     return 0;
