6a0c96f0b4c98cba2a1968dc517a045d7df3e748
Samuel Camilo <ssamuelcamilo@gmail.com>
2018-11-02 01:18:29 -0300

    solução do problema 1149

--- 1149/main.c
+++ 1149/main.c
@@ -3,6 +3,27 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int A;
+    int N;
+    int validador = 0;
+    int cont;
+    int result = 0;
+
+    scanf("%d %d", &A, &N);
+
+    while(validador == 0){
+        if(N > 0){
+            validador = 1;
+        }else{
+            scanf("%d", &N);
+        }
+    }
+
+    for(cont = 1; cont < N; cont++){
+        result += cont + A;
+    }
+
+    printf("%d\n", result + A);
+
     return 0;
 }
